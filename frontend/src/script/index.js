cac6f91f48b6de3cba9888565c402b3661ce4724
niqooll <niqozr01@gmail.com>
2025-06-13 15:18:05 +0700

    fix profile dropdown, dll

--- frontend/src/script/index.js
+++ frontend/src/script/index.js
@@ -1,6 +1,6 @@
 // src/script/index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap'; // PENTING: Pertahankan hanya ini untuk JS Bootstrap
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import '../styles/styles.css'; // Mengimpor CSS kustom Anda
@@ -8,17 +8,17 @@ import logo from '../public/image/logo.png'; // Mengimpor gambar logo
 import App from './app.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const brandLink = document.querySelector('.navbar-brand');
-  if (brandLink) {
-    const logoImg = document.createElement('img');
-    logoImg.src = logo;
-    logoImg.alt = 'CoReer Logo';
-    logoImg.className = 'logo-image';
+    const brandLink = document.querySelector('.navbar-brand');
+    if (brandLink) {
+        const logoImg = document.createElement('img');
+        logoImg.src = logo;
+        logoImg.alt = 'CoReer Logo';
+        logoImg.className = 'logo-image';
 
-    brandLink.innerHTML = ''; 
-    brandLink.appendChild(logoImg); 
-  }
+        brandLink.innerHTML = '';
+        brandLink.appendChild(logoImg);
+    }
 
-  const app = new App();
-  app.init();
+    const app = new App();
+    app.init();
 });
